f2373e8776e3061ddbcc83990236b1020fa4f9eb
Mike Galarneau <mbgalarneau@gmail.com>
2019-02-05 09:51:31 -0500

    basic website, no functionality other than a splash page

---
+++ config/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  "development": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PWD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": "password",
+    "database": "testdb",
+    "host": "localhost",
+    "dialect": "mysql",
+    "logging": false
+  },
+  "production": {
+    "use_env_variable": "JAWSDB_URL",
+    "dialect": "mysql"
+  }
+};
