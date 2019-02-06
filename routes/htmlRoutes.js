1ab355f66b7035e94a553636ef021ac931faeb73
Mike Galarneau <mbgalarneau@gmail.com>
2019-02-06 15:16:18 -0500

    added saved articles page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
 
   // Load index page
   app.get("/", function (req, res) {
-    console.log("Getting articles");
+    console.log("Getting scraped articles");
 
     db.Article.find({})
       .then(function (data) {
@@ -21,7 +21,20 @@ module.exports = function (app) {
   });
 
   app.get("/saved", function (req, res) {
-    res.render("saved");
+    console.log("Getting saved articles");
+
+    db.Article.find({})
+      .then(function (data) {
+        var articleData = {
+          articles: data
+        };
+
+        res.render("saved", articleData);
+      })
+      .catch(function (err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
   });
 
 };
