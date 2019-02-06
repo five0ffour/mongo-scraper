653284c09433ce17273665382498cd054c531f5d
Mike Galarneau <mbgalarneau@gmail.com>
2019-02-06 10:08:56 -0500

    added display of titles to main page with action buttons

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -29,7 +29,6 @@ module.exports = function (app) {
 
         db.Article.create(result)
           .then(function (dbArticle) {
-            console.log(dbArticle);
           })
           .catch(function (err) {
             console.log(err);
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,23 @@
+var db = require("../models");
 
 module.exports = function (app) {
 
   // Load index page
   app.get("/", function (req, res) {
-    res.render("index");
+    console.log("Getting articles");
+
+    db.Article.find({})
+      .then(function (data) {
+        var articleData = {
+          articles: data
+        };
+
+        res.render("index", articleData);
+      })
+      .catch(function (err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
   });
 
   app.get("/saved", function (req, res) {
